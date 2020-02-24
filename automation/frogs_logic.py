5d02112f768443ad9e641762bf5a661e92a41c65
Anastasia Ionicioiu <anastasia@lifecycle.today>
2020-02-24 11:25:26 +0200

    Running a simplest testcase for frogs game
    using Selenium library and unittests

---
+++ automation/frogs_logic.py
@@ -0,0 +1,46 @@
+import json
+from typing import Dict
+
+import jsonpath
+import bs4
+
+
+class FrogsGame():
+    """Self-learning logic for solving frogs game"""
+
+    def __init__(self):
+        # initializing game parameters
+        self.NoPossibleMoves = False
+        self.current_state: Dict[int, int] = {}
+
+    def ReadCurrentState(self, source_code):
+        htmlsource = bs4.BeautifulSoup(source_code)
+        cells = htmlsource.select('img')
+        i: int = 1
+        for img in cells:
+            if "frog0" in str(img):
+                self.current_state[i] = 0
+            if "frog1" in str(img):
+                self.current_state[i] = 1
+            if "frog2" in str(img):
+                self.current_state[i] = 2
+            i = i + 1
+
+    def PrintCurrentState(self):
+        print(self.current_state)
+
+    def NextMove(self, source_code):
+        # read current state from the page source & check if previous move changed the game state
+        self.ReadCurrentState(source_code)
+        # calculate next move
+            # check in the game memory if there is a winning path
+
+            # if no, check in the game memory if there are possible moves (which don't lead to game being lost)
+
+            # if no game memory for self.current_state go to discovery and build the memory
+
+        # assume the game is lost from the game model
+
+        # actual check that there are no possible moves (try all 1 to 9 and see no difference in self.current_state)
+        self.NoPossibleMoves = True
+        # write next move into frog.json
---
+++ automation/frogs_test.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from selenium import webdriver
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
+
+import frogs_logic
+
+import json
+import jsonpath
+
+
+# Starting testcases
+class Frogs(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://www.lutanho.net/play/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+
+    def test_frogs(self):
+        driver = self.driver
+        game = frogs_logic.FrogsGame()
+        driver.get("http://www.lutanho.net/play/frogs.html")
+
+        while (self.is_alert_present() == False ) and (game.NoPossibleMoves == False):
+        # Calculate next movement based on the changes of the game state and write json with next move
+            game.NextMove(driver.page_source)
+            # load next move from frogs.json and execute it
+
+            # driver.find_element_by_xpath("//img[contains(@src,'frog0.gif')]").click()
+
+        if self.is_alert_present():
+            # game is won
+            self.assertIn("Super, you solved this game", self.close_alert_and_get_its_text())
+            # calculate and write the game stats to memory
+
+            print("Game is won")
+        elif game.NoPossibleMoves:
+            # game is lost
+            # No possible moves
+            self.assertEqual(game.NoPossibleMoves, True)
+            # calculate and write the game stats to memory
+
+            print("Game is lost. No possible moves")
+        else:
+            # we're not supposed to get here, unless something went wrong
+            print("Something went wrong")
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to.alert
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to.alert
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
